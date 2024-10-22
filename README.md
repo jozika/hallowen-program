cd9d9effe94a877637cc07a3b0deaccd7cf209c2
Semiro Bule <semiro.bule@elev.ga.ntig.se>
2024-10-22 11:02:57 +0200

    Skapd projekt och Readme fil

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hallowen
+
+## Spel om skräck filmer man kollar under halloween .
+
+Detta spelet består av tre frågor, där man ska svara genom att välja en av de tre filmerna som är står in i inputen . Det är bland annat Hush, Silance och the Nun. 
+
+
+
+![ab67706c0000da8479dc9fdddcdfd5515607dfa0](https://hackmd.io/_uploads/H1sft1Sxkg.jpg)
--- hallowen.py
+++ hallowen.py
@@ -1,1 +1,10 @@
-print ("hej")
+# halloween.py
+
+horror_film = input("which type of horror films do you watch? (hush,silance, nun): ").lower()
+
+if horror_film == "hush": 
+    print ("I see a strong mind")
+elif horror_film == "silance":
+    print ("Woah!! take it easy Jamal, dont pull up the g")
+elif horror_film == "nun":
+    print("we get it you aren't scared, mooaa!!")
